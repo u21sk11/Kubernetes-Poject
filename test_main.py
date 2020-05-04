b30dbef6479dfbaef625f772cefe86b8763660c0
Satas <s.kapoius.11@gmail.com>
2020-05-04 02:09:02 +0100

    testing the build

--- test_main.py
+++ test_main.py
@@ -25,7 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'Heay'
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
